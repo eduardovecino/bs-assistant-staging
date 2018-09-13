bce0da3d5ea41cf6431c89901d228032d5b20538
pturalgi <pturalgi@everis.com>
2018-09-13 13:20:40 +0200

    new dist

--- dist/index-dialogflow.js
+++ dist/index-dialogflow.js
@@ -2,5 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_dialogflow_1 = require("./app-dialogflow");
 new app_dialogflow_1.default();
-console.log('Hola');
 //# sourceMappingURL=index-dialogflow.js.map
