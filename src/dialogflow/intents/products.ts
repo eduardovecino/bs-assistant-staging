672f49c0be12116a3df3133f52d5d2b806f09460
pturalgi <pturalgi@everis.com>
2018-09-13 13:29:36 +0200

    added default welcome intent

--- src/dialogflow/intents/products.ts
+++ src/dialogflow/intents/products.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { ProductService } from "../../services/products.service";
+import { Permission } from "../../../node_modules/actions-on-google";
 
 export class ProductIntents {
 
@@ -10,6 +11,13 @@ export class ProductIntents {
     }
 
     public intents(app): void {
+        app.intent('Default Welcome Intent', conv => {
+            conv.ask(new Permission({
+                context: 'Para dirigirme a usted por su nombre y conocer su ubicación,',
+                permissions: [],
+            }));
+        });
+
         app.intent('Saldo cuenta', (conv, { tipo_cuenta }) => {
             const data = this.productsService.getProducts();
             conv.ask('El saldo  de la cuenta es de ' + data.num);
