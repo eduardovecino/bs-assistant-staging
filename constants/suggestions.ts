5f5088d4942aacf5dd80a8053062572810850291
Eduardo Vecino <eduardo.vecino.gonzalez@everis.com>
2018-09-14 11:23:39 +0200

    test edus

--- constants/suggestions.ts
+++ constants/suggestions.ts
@@ -1,11 +1,6 @@
 export const SUGGESTIONS = {
     NOT_LOGGED_SUGGESTIONS: [
-        'Iniciar Sesión',
-        'Oficinas Cercanas',
-        'Abrir App'],
+        'Iniciar Sesión'],
     LOGGED_SUGGESTIONS: [
-        'Cuentas',
-        'Tarjetas',
-        'Oficinas Cercanas',
-        'Abrir App']
+        'Iniciar Sesión']
 }
