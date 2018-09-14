9f37230b8affd656d935584cadf8b11e18760359
pturalgi <pturalgi@everis.com>
2018-09-14 14:12:21 +0200

    tests

--- dist/dialogflow/intents/products.js
+++ dist/dialogflow/intents/products.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const actions_on_google_1 = require("actions-on-google");
 const products_service_1 = require("../../services/products.service");
 const base_intents_1 = require("./base-intents");
 class ProductIntents extends base_intents_1.BaseIntents {
@@ -10,53 +9,51 @@ class ProductIntents extends base_intents_1.BaseIntents {
     }
     intents(app) {
         console.log('Registering Products Intents');
-        // app.intent('Default Welcome Intent', conv => {
-        //     conv.ask(`Bienvenido a Banco Sabadell`); 
-        //     this.uggestions(conv);       
-        // });
         app.intent('Default Welcome Intent', conv => {
-            conv.ask(new actions_on_google_1.Permission({
-                context: 'Para dirigirme a usted por su nombre y conocer su ubicación,',
-                permissions: ['NAME', 'DEVICE_PRECISE_LOCATION', 'DEVICE_COARSE_LOCATION'],
-            }));
-        });
-        // Create a Dialogflow intent with the `actions_intent_PERMISSION` event
-        app.intent('Get Permission', (conv, params, confirmationGranted) => {
             conv.ask(`Bienvenido a Banco Sabadell`);
-            const { name } = conv.user;
-            if (confirmationGranted) {
-                if (name) {
-                    conv.ask(`Bienvenido a Banco Sabadell, ${name.display}`);
-                    this.suggestions(conv);
-                }
-            }
-            else {
-                conv.ask(`I can't read your mind right now! My mystical powers have failed!`);
-            }
+            // this.suggestions(conv);       
         });
-        //Iniciar Sesión
-        app.intent('Iniciar Sesion', (conv) => {
-            conv.ask(`Vamos a iniciar sesión`);
-            conv.ask(new actions_on_google_1.SignIn());
-        });
-        app.intent('Get Signin', (conv, params, signin) => {
-            this.logged = '1'; //TEST
-            if (signin.status === 'OK') {
-                const access = conv.user.access.token; //possibly do something with access token
-                conv.ask(`¡Genial, gracias por iniciar sesión! ${access}`);
-                this.suggestions(conv);
-            }
-            else {
-                //${signin.status}
-                conv.ask(`No podré guardar tus datos, pero ¿qué quieres hacer a continuación?`);
-                this.suggestions(conv);
-            }
-        });
-        app.intent('Saldo cuenta', (conv, { tipo_cuenta }) => {
-            console.log('INTENT: Saldo cuenta');
-            const data = this.productsService.getProducts();
-            conv.ask('El saldo  de la cuenta es de ' + data.num);
-        });
+        // app.intent('Default Welcome Intent', conv => {
+        //     conv.ask(new Permission({
+        //         context: 'Para dirigirme a usted por su nombre y conocer su ubicación,',
+        //         permissions: ['NAME', 'DEVICE_PRECISE_LOCATION', 'DEVICE_COARSE_LOCATION'],
+        //     }));
+        // });
+        // // Create a Dialogflow intent with the `actions_intent_PERMISSION` event
+        // app.intent('Get Permission', (conv, params, confirmationGranted) => {
+        //     conv.ask(`Bienvenido a Banco Sabadell`);
+        //     const { name } = conv.user;
+        //     if (confirmationGranted) {
+        //         if (name) {
+        //             conv.ask(`Bienvenido a Banco Sabadell, ${name.display}`);
+        //             this.suggestions(conv);
+        //         }
+        //     } else {
+        //         conv.ask(`I can't read your mind right now! My mystical powers have failed!`);
+        //     }
+        // });
+        // //Iniciar Sesión
+        // app.intent('Iniciar Sesion', (conv) => {
+        //     conv.ask(`Vamos a iniciar sesión`);
+        //     conv.ask(new SignIn());
+        // });
+        // app.intent('Get Signin', (conv, params, signin) => {
+        //     this.logged = '1'; //TEST
+        //     if (signin.status === 'OK') {
+        //         const access = conv.user.access.token;  //possibly do something with access token
+        //         conv.ask(`¡Genial, gracias por iniciar sesión! ${access}`);
+        //         this.suggestions(conv);
+        //     } else {
+        //         //${signin.status}
+        //         conv.ask(`No podré guardar tus datos, pero ¿qué quieres hacer a continuación?`);
+        //         this.suggestions(conv);
+        //     }
+        // });
+        // app.intent('Saldo cuenta', (conv, { tipo_cuenta }) => {
+        //     console.log('INTENT: Saldo cuenta');
+        //     const data = this.productsService.getProducts();
+        //     conv.ask('El saldo  de la cuenta es de ' + data.num);
+        // });
     }
 }
 exports.ProductIntents = ProductIntents;
--- src/dialogflow/intents/products.ts
+++ src/dialogflow/intents/products.ts
@@ -14,55 +14,55 @@ export class ProductIntents extends BaseIntents {
     public intents(app): void {
         console.log('Registering Products Intents');
 
+        app.intent('Default Welcome Intent', conv => {
+            conv.ask(`Bienvenido a Banco Sabadell`); 
+            // this.suggestions(conv);       
+        });
+
         // app.intent('Default Welcome Intent', conv => {
-        //     conv.ask(`Bienvenido a Banco Sabadell`); 
-        //     this.uggestions(conv);       
+        //     conv.ask(new Permission({
+        //         context: 'Para dirigirme a usted por su nombre y conocer su ubicación,',
+        //         permissions: ['NAME', 'DEVICE_PRECISE_LOCATION', 'DEVICE_COARSE_LOCATION'],
+        //     }));
         // });
 
-        app.intent('Default Welcome Intent', conv => {
-            conv.ask(new Permission({
-                context: 'Para dirigirme a usted por su nombre y conocer su ubicación,',
-                permissions: ['NAME', 'DEVICE_PRECISE_LOCATION', 'DEVICE_COARSE_LOCATION'],
-            }));
-        });
+        // // Create a Dialogflow intent with the `actions_intent_PERMISSION` event
+        // app.intent('Get Permission', (conv, params, confirmationGranted) => {
+        //     conv.ask(`Bienvenido a Banco Sabadell`);
+        //     const { name } = conv.user;
+        //     if (confirmationGranted) {
+        //         if (name) {
+        //             conv.ask(`Bienvenido a Banco Sabadell, ${name.display}`);
+        //             this.suggestions(conv);
+        //         }
+        //     } else {
+        //         conv.ask(`I can't read your mind right now! My mystical powers have failed!`);
+        //     }
+        // });
 
-        // Create a Dialogflow intent with the `actions_intent_PERMISSION` event
-        app.intent('Get Permission', (conv, params, confirmationGranted) => {
-            conv.ask(`Bienvenido a Banco Sabadell`);
-            const { name } = conv.user;
-            if (confirmationGranted) {
-                if (name) {
-                    conv.ask(`Bienvenido a Banco Sabadell, ${name.display}`);
-                    this.suggestions(conv);
-                }
-            } else {
-                conv.ask(`I can't read your mind right now! My mystical powers have failed!`);
-            }
-        });
+        // //Iniciar Sesión
+        // app.intent('Iniciar Sesion', (conv) => {
+        //     conv.ask(`Vamos a iniciar sesión`);
+        //     conv.ask(new SignIn());
+        // });
 
-        //Iniciar Sesión
-        app.intent('Iniciar Sesion', (conv) => {
-            conv.ask(`Vamos a iniciar sesión`);
-            conv.ask(new SignIn());
-        });
+        // app.intent('Get Signin', (conv, params, signin) => {
+        //     this.logged = '1'; //TEST
+        //     if (signin.status === 'OK') {
+        //         const access = conv.user.access.token;  //possibly do something with access token
+        //         conv.ask(`¡Genial, gracias por iniciar sesión! ${access}`);
+        //         this.suggestions(conv);
+        //     } else {
+        //         //${signin.status}
+        //         conv.ask(`No podré guardar tus datos, pero ¿qué quieres hacer a continuación?`);
+        //         this.suggestions(conv);
+        //     }
+        // });
 
-        app.intent('Get Signin', (conv, params, signin) => {
-            this.logged = '1'; //TEST
-            if (signin.status === 'OK') {
-                const access = conv.user.access.token;  //possibly do something with access token
-                conv.ask(`¡Genial, gracias por iniciar sesión! ${access}`);
-                this.suggestions(conv);
-            } else {
-                //${signin.status}
-                conv.ask(`No podré guardar tus datos, pero ¿qué quieres hacer a continuación?`);
-                this.suggestions(conv);
-            }
-        });
-
-        app.intent('Saldo cuenta', (conv, { tipo_cuenta }) => {
-            console.log('INTENT: Saldo cuenta');
-            const data = this.productsService.getProducts();
-            conv.ask('El saldo  de la cuenta es de ' + data.num);
-        });
+        // app.intent('Saldo cuenta', (conv, { tipo_cuenta }) => {
+        //     console.log('INTENT: Saldo cuenta');
+        //     const data = this.productsService.getProducts();
+        //     conv.ask('El saldo  de la cuenta es de ' + data.num);
+        // });
     }
 }
